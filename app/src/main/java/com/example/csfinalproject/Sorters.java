cc6dc9564683bb9d44b59690f523d6d769de85e4
Trevor Gordley <gordley2@illinois.edu>
2019-04-20 14:45:58 -0500

    Added text box, made recommendations bar functional

---
+++ app/src/main/java/com/example/csfinalproject/Sorters.java
@@ -0,0 +1,45 @@
+package com.example.csfinalproject;
+
+public class Sorters {
+    // This is where we'll put all the sorting functions. I hope to get this done by the end of the weekend.
+    //Other goals include getting the sample text button up and making the text box functional.
+    //That should make the app minimally functional.
+
+    /**
+     *
+     * @param text
+     * @return words sorted in alphabetical order
+     */
+    public String alphabeticalSort(String text) {
+        return null;
+    }
+
+    /**
+     *
+     * @param text
+     * @return words sorted by word length, shortest to longest
+     */
+    public String lengthSort(String text) {
+        return null;
+    }
+
+    /**
+     *
+     * @param text
+     * @return the words sorted by the sum of the binary values of their characters, least to greatest.
+     */
+    public String binarySort(String text) {
+        return null;
+
+    }
+
+    /**
+     *
+     * @param text
+     * @return sentences sorted in order of their ending punctuation. The order is ?!.
+     */
+    public String punctuationSort(String text) {
+        return null;
+    }
+    // I want at least on more thing to sort by. Ideas?
+}
